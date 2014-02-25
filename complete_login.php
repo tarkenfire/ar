0e2071e4e92022970054ac23c9fbea28693827b1
Michael Mancuso <tarkenfire@tarkenfire.com>
2014-02-24 22:56:35 -0500

    Minor work on login.

--- complete_login.php
+++ complete_login.php
@@ -6,9 +6,15 @@
 	* Twitter login script.
 	*/
 
+	require ("serverscripts/twitteroauth.php");
+	session_start();
+
 	//Check for needed info from POST. If not found, return to login script.
 	if (!empty($_GET['oauth_verifier']) && !empty($_GET['oauth_token']) && !empty($_GET['oauth_token_secret'])){
-		//TODO logic.
+		//TODO: Key/Secret
+		$oauthsession = new TwitterOAuth('', '', $_SESSION['oauth_token'], $_SESSION['oauth_token_secret'] );
+		$access_token = $twitteroauth->getAccessToken($_GET['oauth_verifier']);
+		$_SESSION['access_token'] = $access_token;
 	}
 	else{
 		header('Location: login.php');
